b5e977dde5faa9aa297f309a26ccb0702253cd5d
Renate <renate@salad.ventures>
2022-07-23 14:38:26 +0800

    Implement JWT strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { JWT_CONSTANTS } from './constants';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: JWT_CONSTANTS.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
