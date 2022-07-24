6cf2e5a4d23d2e932ec6400a9e405ea0228c0775
xXValhallaXx <renate.gouveia@gmail.com>
2022-07-24 18:32:58 +0800

    Work on sequlize migrations

---
+++ src/database/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'producto',
+  },
+  test: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'producto',
+  },
+  production: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'producto',
+  },
+};
