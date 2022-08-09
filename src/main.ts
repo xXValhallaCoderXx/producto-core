cbf01f1a46c944d505e2d3076e9aef31c12b786b
Renate <renate@salad.ventures>
2022-08-09 22:14:12 +0800

    Transform validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors) => new BadRequestException(errors),
+    }),
+  );
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
