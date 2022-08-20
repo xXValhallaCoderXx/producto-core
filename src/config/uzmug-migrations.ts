3715e7e60c096d3eb4cbbb75f9b70ac7c895002e
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-08-20 17:28:29 +0800

    setup uzmug to handle migration and seeds

---
+++ src/config/uzmug-migrations.ts
@@ -0,0 +1,60 @@
+import 'dotenv/config';
+import path = require('path');
+import fs from 'fs';
+import { Sequelize } from 'sequelize';
+import { Umzug, SequelizeStorage } from 'umzug';
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+(async () => {
+  const umzug = new Umzug({
+    migrations: {
+      glob: path.resolve(__dirname, '../database/migrations/*.ts'),
+    },
+    context: sequelize.getQueryInterface(),
+    storage: new SequelizeStorage({ sequelize }),
+    logger: console,
+    create: {
+      folder: path.resolve(__dirname, './database/migrations'),
+      template: (filepath) => [
+        [
+          filepath,
+          fs
+            .readFileSync(path.join(__dirname, './umzug-template.ts'))
+            .toString(),
+        ],
+      ],
+    },
+  });
+  const action = process.argv[2];
+  if (!action) {
+    throw new Error('Action is required up | down');
+  }
+  if (action === 'up') {
+    console.log('Running Migrations');
+    await umzug.up();
+  }
+
+  if (action === 'down') {
+    console.log('Running Migrations');
+    await umzug.down();
+  }
+
+  if (action === 'create') {
+    const name = process.argv[3];
+    if (!name) {
+      throw new Error('Please include a name');
+    }
+    console.log('Creating Migration');
+    await umzug.create({ name: `${name}.ts` });
+  }
+})().then(() => process.exit());
+
+// export default umzug;
